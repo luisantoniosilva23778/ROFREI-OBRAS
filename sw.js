08de198f71d8313be94d640dc2361accd0cba00e
luisantoniosilva23778 <luisantoniosilva23778@gmail.com>
2026-03-16 08:20:57 +0000

    Implement cache management in service worker
    Added cache management for service worker installation and fetch events.

--- sw.js
+++ sw.js
@@ -1,3 +1,22 @@
+// Este nombre se genera solo con la fecha para forzar la actualización si es necesario
+const CACHE_NAME = 'rofreicrc-' + new Date().getTime();
+const assets = ['./', './index.html', './manifest.json'];
+
 self.addEventListener('install', e => {
-console.log("ROFREICRC app instalada");
+    // Elimina cachés viejos automáticamente para que no den error 404
+    e.waitUntil(
+        caches.keys().then(keys => {
+            return Promise.all(keys
+                .filter(key => key !== CACHE_NAME)
+                .map(key => caches.delete(key))
+            );
+        })
+    );
+    self.skipWaiting();
+});
+
+self.addEventListener('fetch', e => {
+    e.respondWith(
+        fetch(e.request).catch(() => caches.match(e.request))
+    );
 });
